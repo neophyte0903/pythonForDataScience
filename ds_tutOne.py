20b1b38c23efffdcacebf4f79d49a5f15a439040
gagandeep singh <gagandeep.21gcebaids001@galgotiacollege.edu>
2024-02-13 22:32:22 +0530

    tutOne added

---
+++ ds_tutOne.py
@@ -0,0 +1,23 @@
+rent = 1220
+gas = 202.5
+groceries = 305.5
+
+
+print(rent+gas+groceries)
+# cannot use python keywords as variable name
+# variable cannot start from digit
+# only underscore
+
+
+'''numbers'''
+
+nyc_bal = 188
+bal_pitt = 247
+
+total_distance = nyc_bal+bal_pitt
+speed = 65
+
+time = total_distance/speed
+print(round(time, 2))
+
+# python follows BODMAS and PEMDAS
